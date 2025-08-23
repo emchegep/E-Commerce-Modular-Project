50d6f79316a4956476287ba46b78292c6e7efe5f
Peter Chege <emchegep@gmail.com>
2025-08-23 20:36:37 +0300

    Added OrderServiceProvider and configured the migrations, routes and configs

---
+++ modules/Order/config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'order_status' => [
+        'pending' => 'Pending',
+        'processing' => 'Processing',
+        'completed' => 'Completed',
+        'canceled' => 'Canceled',
+    ],
+    'max_items_per_order' => 5
+];
